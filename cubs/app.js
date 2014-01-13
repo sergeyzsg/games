271e26b4bf36d7b4810d331b24bc3184af0f82b9
Sergey Chuprakov <sergeyzsg@yandex.ru>
2014-01-13 15:12:30 +0600

    simplewebrtc.js

--- cubs/app.js
+++ cubs/app.js
@@ -31,7 +31,99 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
+var server = http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
   console.log('http://localhost:' + app.get('port') + '/');
+});
+
+/*global console*/
+var yetify = require('yetify'),
+    uuid = require('node-uuid'),
+    io = require('socket.io').listen(server);
+
+function describeRoom(name) {
+    var clients = io.sockets.clients(name);
+    var result = {
+        clients: {}
+    };
+    clients.forEach(function (client) {
+        result.clients[client.id] = client.resources;
+    });
+    return result;
+}
+
+function safeCb(cb) {
+    if (typeof cb === 'function') {
+        return cb;
+    } else {
+        return function () {};
+    }
+}
+
+io.sockets.on('connection', function (client) {
+    client.resources = {
+        screen: false,
+        video: true,
+        audio: false
+    };
+
+    // pass a message to another id
+    client.on('message', function (details) {
+        var otherClient = io.sockets.sockets[details.to];
+        if (!otherClient) return;
+        details.from = client.id;
+        otherClient.emit('message', details);
+    });
+
+    client.on('shareScreen', function () {
+        client.resources.screen = true;
+    });
+
+    client.on('unshareScreen', function (type) {
+        client.resources.screen = false;
+        if (client.room) removeFeed('screen');
+    });
+
+    client.on('join', join);
+
+    function removeFeed(type) {
+        io.sockets.in(client.room).emit('remove', {
+            id: client.id,
+            type: type
+        });
+    }
+
+    function join(name, cb) {
+        // sanity check
+        if (typeof name !== 'string') return;
+        // leave any existing rooms
+        if (client.room) removeFeed();
+        safeCb(cb)(null, describeRoom(name))
+        client.join(name);
+        client.room = name;
+    }
+
+    // we don't want to pass "leave" directly because the
+    // event type string of "socket end" gets passed too.
+    client.on('disconnect', function () {
+        removeFeed();
+    });
+    client.on('leave', removeFeed);
+
+    client.on('create', function (name, cb) {
+        if (arguments.length == 2) {
+            cb = (typeof cb == 'function') ? cb : function () {};
+            name = name || uuid();
+        } else {
+            cb = name;
+            name = uuid();
+        }
+        // check if exists
+        if (io.sockets.clients(name).length) {
+            safeCb(cb)('taken');
+        } else {
+            join(name);
+            safeCb(cb)(null, name);
+        }
+    });
 });

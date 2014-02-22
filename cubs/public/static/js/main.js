ea7b7ed174608e818163d9fe6b1d07205beab5d2
Sergey Chuprakov <sergeyzsg@yandex.ru>
2014-02-22 10:46:08 +0600

    Load rules

--- cubs/public/static/js/main.js
+++ cubs/public/static/js/main.js
@@ -29,7 +29,7 @@ define(['game', 'simplewebrtc'], function() {
     }
 
     $(function() {
-        startWebRTC();
+        //startWebRTC();
     });
 
     $(function() {
--- cubs/routes/index.js
+++ cubs/routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var path = require('path');
+var fs = require('fs');
+
+exports.index = function(req, res) {
+    res.render('index', {title: 'Игра Кубики'});
+};
+
+exports.rules = function(req, res) {
+    var rulesHTMLpath = path.normalize(path.join(path.normalize(__dirname), '../public/static/html/slava/rules2.html'));
+
+    res.render('rules', {title: 'Правила', content: fs.readFileSync(rulesHTMLpath, {encoding: 'utf8'})});
 };
